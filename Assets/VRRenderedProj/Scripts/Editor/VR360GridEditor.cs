645931ae452bbef19a3c192458fa26c9d8e7d475
Koki Ibukuro <koki.ibukuro@gmail.com>
2017-02-06 15:30:28 +0900

    Export to file

--- Assets/VRRenderedProj/Scripts/Editor/VR360GridEditor.cs
+++ Assets/VRRenderedProj/Scripts/Editor/VR360GridEditor.cs
@@ -21,11 +21,12 @@ public class VR360GridEditor : Editor
             Handles.Label(pos, "Pos" + i);
             if (pos != newPos)
             {
-                newPos = findPositiontOnProjectionFromCamera(newPos, grid.OffsetFromSphere);
+                newPos = FindPositiontOnProjectionFromCamera(newPos, grid.OffsetFromSphere);
                 positions[i] = newPos;
             }
         }
 
+        // Undo
         switch (eventType)
         {
             case EventType.MouseDown:
@@ -38,146 +39,35 @@ public class VR360GridEditor : Editor
                 break;
         }
 
-        if (grid.GetComponent<MeshFilter>() != null)
-            CreateGrid();
-
-        if (grid.positions.Count == 4)
+        if (positions.Count == 4)
         {
-            Handles.DrawLine(grid.positions[0], grid.positions[1]);
-            Handles.DrawLine(grid.positions[1], grid.positions[2]);
-            Handles.DrawLine(grid.positions[2], grid.positions[3]);
-            Handles.DrawLine(grid.positions[3], grid.positions[0]);
+            grid.CreateGrid();
+            Handles.DrawLine(positions[0], positions[1]);
+            Handles.DrawLine(positions[1], positions[2]);
+            Handles.DrawLine(positions[2], positions[3]);
+            Handles.DrawLine(positions[3], positions[0]);
 
             for (int i = 0; i < grid.width + 1; i++)
             {
                 for (int j = 0; j < grid.height + 1; j++)
                 {
-                    Handles.DotCap(0, GetPos(i, j), Quaternion.identity, 0.005f);
+                    Handles.DotCap(0, grid.GetPos(i, j), Quaternion.identity, 0.005f);
                 }
             }
         }
-    }
-
-    Vector3 GetPos(int i, int j)
-    {
-        float hor = (i * 1.0f) / grid.width;
-        float ver = (j * 1.0f) / grid.height;
-
-        Vector3 A = grid.positions[0] + (grid.positions[1] - grid.positions[0]) * hor;
-        Vector3 B = grid.positions[3] + (grid.positions[2] - grid.positions[3]) * hor;
-
-        Vector3 P = A + (B - A) * ver;
-
-        if (grid.ProjectOnSphere)
-        {
-            return findPositiontOnProjection(P, grid.OffsetFromSphere);
-        }
-        else
-        {
-            return P;
-        }
-
-    }
-
-    void CreateGrid(bool forceCreate = false)
-    {
-        MeshFilter meshFilter = grid.gameObject.GetComponent<MeshFilter>();
-        if (meshFilter == null)
-        {
-            meshFilter = grid.gameObject.AddComponent<MeshFilter>();
-            grid.gameObject.AddComponent<MeshRenderer>();
-        }
-
-        Mesh mesh = new Mesh();
-        meshFilter.mesh = mesh;
-
-        int w = grid.width + 1;
-        int h = grid.height + 1;
-
-        Vector3[] vertices = new Vector3[(w) * (h)];
-        int[] tri = new int[(w - 1) * (h - 1) * 6];
-        Vector3[] normals = new Vector3[(w) * (h)];
-        Vector2[] uv = new Vector2[(w) * (h)];
-
-        int triInd = 0;
-        for (int j = 0; j < h; j++)
-        {
-            for (int i = 0; i < w; i++)
-            {
-                int ind = i + j * w;
-
-                vertices[ind] = GetPos(i, j);
-                normals[ind] = -Vector3.forward;
-                uv[ind] = new Vector2((i * 1.0f) / (w - 1), 1.0f - (j * 1.0f) / (h - 1));
-
-                if (j < grid.height && i < grid.width)
-                {
-
-                    tri[triInd * 6 + 0] = i + j * w;
-                    tri[triInd * 6 + 1] = (i + 1) + j * w;
-                    tri[triInd * 6 + 2] = (i + 1) + (j + 1) * w;
-
-                    tri[triInd * 6 + 3] = i + j * w;
-                    tri[triInd * 6 + 4] = (i + 1) + (j + 1) * w;
-                    tri[triInd * 6 + 5] = i + (j + 1) * w;
-
-                    triInd++;
-                }
-            }
-        }
-
-        mesh.vertices = vertices;
-        mesh.triangles = tri;
-        mesh.normals = normals;
-        mesh.uv = uv;
-
-        /*uv[0] = new Vector2(0, 0);
-        uv[1] = new Vector2(1, 0);
-        uv[2] = new Vector2(0, 1);
-        uv[3] = new Vector2(1, 1);*/
     }
 
     public override void OnInspectorGUI()
     {
-        this.DrawDefaultInspector();
+        DrawDefaultInspector();
 
-        for (int i = 0; i < grid.positions.Count; i++)
+        if (GUILayout.Button("Save mesh to file"))
         {
-            GUILayout.BeginHorizontal();
-            GUILayout.Label("Pos" + i);
-            if (GUILayout.Button("X"))
-            {
-                grid.positions.RemoveAt(i);
-            }
-            GUILayout.EndHorizontal();
-        }
-
-        if (GUILayout.Button("Add Position"))
-        {
-            Vector3 pos = new Vector3(0, 0, 0);
-            if (grid.positions.Count > 0)
-            {
-                Vector3 last = grid.positions[grid.positions.Count - 1];
-                pos = last;
-            }
-            grid.positions.Add(pos);
-        }
-
-        if (GUILayout.Button("Clear All Positions"))
-        {
-            grid.ClearAllPosition();
-        }
-
-        GUILayout.Label("Grid creating", EditorStyles.boldLabel);
-        GUILayout.Label("Can Create: " + (grid.positions.Count == 4));
-
-        if (grid.positions.Count == 4 && GUILayout.Button("CreateGrid"))
-        {
-            CreateGrid(true);
+            SaveMeshToFile();
         }
     }
 
-    Vector3 findPositiontOnProjectionFromCamera(Vector3 pos, float offset = 0f)
+    static Vector3 FindPositiontOnProjectionFromCamera(Vector3 pos, float offset = 0f)
     {
         Camera cam = SceneView.lastActiveSceneView.camera;
 
@@ -203,20 +93,16 @@ public class VR360GridEditor : Editor
         }
     }
 
-    Vector3 findPositiontOnProjection(Vector3 pos, float offset = 0f)
+    void SaveMeshToFile()
     {
-        Ray ray = new Ray(Vector3.zero, pos);
-        RaycastHit hit;
+        var filter = grid.GetComponent<MeshFilter>();
+        var mesh = filter.sharedMesh;
 
-        if (Physics.Raycast(ray, out hit, Mathf.Infinity, 1 << 31))
-        {
-            Vector3 dir = (hit.transform.position - hit.point).normalized;
-            return hit.point + dir * offset;
-        }
-        else
-        {
-            return pos;
-        }
+        var path = EditorUtility.SaveFilePanelInProject("Save mesh", grid.name, "asset", "Select the save path");
+        Debug.LogFormat("path : {0}", path);
+
+        AssetDatabase.CreateAsset(mesh, path);
+        AssetDatabase.SaveAssets();
     }
 
     // for removing default handle
@@ -226,6 +112,17 @@ public class VR360GridEditor : Editor
         grid = (VR360Grid)target;
         LastTool = Tools.current;
         Tools.current = Tool.None;
+
+        if (grid.positions.Count.Equals(0))
+        {
+            grid.positions = new List<Vector3>()
+            {
+                new Vector3(-0.2f, 0.2f, 0.9f),
+                new Vector3(0.2f, 0.2f, 0.9f),
+                new Vector3(0.2f, -0.2f, 0.9f),
+                new Vector3(-0.2f, -0.2f, 0.9f),
+            };
+        }
     }
 
     void OnDisable()
--- Assets/VRRenderedProj/Scripts/VR360Grid.cs
+++ Assets/VRRenderedProj/Scripts/VR360Grid.cs
@@ -1,7 +1,7 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(MeshFilter), typeof(MeshRenderer))]
 public class VR360Grid : MonoBehaviour
 {
     public float OffsetFromSphere = 0f;
@@ -14,10 +14,90 @@ public class VR360Grid : MonoBehaviour
 
     public bool ProjectOnSphere = true;
 
-    public Mesh mesh = null;
-
-    public void ClearAllPosition()
+    public void CreateGrid()
     {
-        positions = new List<Vector3>();
+        MeshFilter meshFilter = GetComponent<MeshFilter>();
+        Mesh mesh = new Mesh();
+        meshFilter.mesh = mesh;
+
+        int w = width + 1;
+        int h = height + 1;
+
+        Vector3[] vertices = new Vector3[(w) * (h)];
+        int[] tri = new int[(w - 1) * (h - 1) * 6];
+        Vector3[] normals = new Vector3[(w) * (h)];
+        Vector2[] uv = new Vector2[(w) * (h)];
+
+        int triInd = 0;
+        for (int j = 0; j < h; j++)
+        {
+            for (int i = 0; i < w; i++)
+            {
+                int ind = i + j * w;
+
+                vertices[ind] = GetPos(i, j);
+                normals[ind] = -Vector3.forward;
+                uv[ind] = new Vector2((i * 1.0f) / (w - 1), 1.0f - (j * 1.0f) / (h - 1));
+
+                if (j < height && i < width)
+                {
+                    tri[triInd * 6 + 0] = i + j * w;
+                    tri[triInd * 6 + 1] = (i + 1) + j * w;
+                    tri[triInd * 6 + 2] = (i + 1) + (j + 1) * w;
+
+                    tri[triInd * 6 + 3] = i + j * w;
+                    tri[triInd * 6 + 4] = (i + 1) + (j + 1) * w;
+                    tri[triInd * 6 + 5] = i + (j + 1) * w;
+
+                    triInd++;
+                }
+            }
+        }
+
+        mesh.vertices = vertices;
+        mesh.triangles = tri;
+        mesh.normals = normals;
+        mesh.uv = uv;
+
+        /*uv[0] = new Vector2(0, 0);
+        uv[1] = new Vector2(1, 0);
+        uv[2] = new Vector2(0, 1);
+        uv[3] = new Vector2(1, 1);*/
+    }
+
+    public Vector3 GetPos(int i, int j)
+    {
+        float hor = (i * 1.0f) / width;
+        float ver = (j * 1.0f) / height;
+
+        Vector3 A = positions[0] + (positions[1] - positions[0]) * hor;
+        Vector3 B = positions[3] + (positions[2] - positions[3]) * hor;
+
+        Vector3 P = A + (B - A) * ver;
+
+        if (ProjectOnSphere)
+        {
+            return FindPositiontOnProjection(P, OffsetFromSphere);
+        }
+        else
+        {
+            return P;
+        }
+    }
+
+    static Vector3 FindPositiontOnProjection(Vector3 pos, float offset = 0f)
+    {
+        Ray ray = new Ray(Vector3.zero, pos);
+        RaycastHit hit;
+
+        if (Physics.Raycast(ray, out hit, Mathf.Infinity, 1 << 31))
+        {
+            Vector3 dir = (hit.transform.position - hit.point).normalized;
+            return hit.point + dir * offset;
+        }
+        else
+        {
+            return pos;
+        }
     }
 }
